11f310e28764155bb2b2181d8fe4eae5bef634a3
Abhishek Kumar <>
2021-04-07 07:50:51 +0000

    Band Name Genrator

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Genrator.")
+city = input("Name of City where you grow.. \n")
+pet_name = input("What's your first pet name? \n")
+print("Congratulation ! You have a Band Name ",'"',city +" ",  pet_name,'"')
+
