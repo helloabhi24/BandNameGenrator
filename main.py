8d83d72b64fa4dcdbc6d6506b89654285f2257e0
Abhishek Kumar <40750418+helloabhi24@users.noreply.github.com>
2021-04-07 23:26:58 +0530

    Leap Year

--- main.py
+++ main.py
@@ -46,3 +46,15 @@ if no%2==0:
     print(f"{no} is a even no.")
 else:
     print(f"{no} is a odd no.")
+    
+#Leap Year
+print("Leap Year Calculator!")
+year = int(input("Enter a year. "))
+
+if year%4 ==0:
+    if year%100!=0:
+        print(f"{year} is a leap year")
+    elif year%400==0:
+            print(f"{year} is a Leap Year")
+else:
+    print("Not a leap year")
