3abd89ebf651ecc6a02732a6d5d1bed84a2b2c01
Abhishek Kumar <>
2021-04-07 11:02:09 +0000

    bmi calculator

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+#Band Name Genrator
 print("Welcome to Band Name Genrator.")
 city = input("Name of City where you grow.. \n")
 pet_name = input("What's your first pet name? \n")
 print("Congratulation ! You have a Band Name ",'"',city +" ",  pet_name,'"')
+
+#BMI Calculator
+w = int(input("Enter your w in kg"))
+h = float(input("Enter your h in m"))
+cal = int(w / (h**2))
+print("Bmi is ",cal)
+
 
